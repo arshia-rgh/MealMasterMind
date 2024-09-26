39fde845c3a643969299903cc1a13a7b58afeb09
arshia <arshiarezagholi1212@gmail.com>
2024-09-27 01:41:15 +0330

    added database connection configs

---
+++ user/app/config.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DBConfig:
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
---
+++ user/app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from config import DBConfig
+
+DB_URL = f"mysql+pymysql://{DBConfig.DB_USER}:{DBConfig.DB_PASSWORD}@{DBConfig.DB_HOST}:{DBConfig.DB_PORT}/{DBConfig.DB_NAME}"
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
