a79e229ae2c7d74bb96d387ed1973393e16f6ccb
arshia <arshiarezagholi1212@gmail.com>
2024-09-27 20:01:52 +0330

    changed the create_user service to store hashed password

--- user/app/services/user.py
+++ user/app/services/user.py
@@ -2,15 +2,17 @@ from sqlalchemy.orm import Session
 
 from user.app.models.user import User
 from user.app.schemas.user import RegisterUser, ResponseUser
+from user.app.utils import hash_password
 
 
 def create_user(db: Session, user: RegisterUser) -> ResponseUser:
+    hashed_password = hash_password.hash_password(user.password)
     db_user = User(
         first_name=user.first_name,
         last_name=user.last_name,
         username=user.username,
         email=user.email,
-        password=user.password,
+        password=hashed_password,
         phone_number=user.phone_number
     )
 
