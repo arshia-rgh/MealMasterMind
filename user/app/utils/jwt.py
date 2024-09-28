c554aa80206f1a5032cdfdc25d05492fc5dd30e3
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 14:08:14 +0330

    implemented verify access token

--- user/app/utils/jwt.py
+++ user/app/utils/jwt.py
@@ -21,4 +21,9 @@ def create_access_token(data: dict):
 
 
 def verify_access_token(token: str):
-    pass
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        return payload
+
+    except jwt.PyJWTError:
+        return None
