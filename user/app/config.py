15229a5d2f7415c366b0d1c39f91101a27541b92
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 13:11:26 +0330

    added secret key and jwt configs

--- user/app/config.py
+++ user/app/config.py
@@ -11,3 +11,8 @@ class DBConfig:
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 20
