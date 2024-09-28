320e726058b7f8805eb22d17c5b344d58b7d04d7
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 18:57:01 +0330

    changed tokenurl of oauth_scheme to /api/login/

--- user/app/dependencies.py
+++ user/app/dependencies.py
@@ -6,7 +6,7 @@ from user.app.database import get_db
 from user.app.models.user import User
 from user.app.utils.jwt import verify_access_token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login/")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
