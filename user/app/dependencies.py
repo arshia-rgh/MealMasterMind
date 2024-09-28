cf806bac4a9711401601fa0e5675b781d749da87
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 15:11:43 +0330

    added get current user dependency

---
+++ user/app/dependencies.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from user.app.database import get_db
+from user.app.models.user import User
+from user.app.utils.jwt import verify_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    payload = verify_access_token(token)
+
+    if payload is None:
+        raise credentials_exception
+
+    username = payload.get("sub")
+
+    if username is None:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.username == username).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+
+    return user
