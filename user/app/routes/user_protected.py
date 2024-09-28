ea6ed5805daa4f338ebe842d2f68b1e11a589d3b
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 19:08:25 +0330

    added delete user to the routes

--- user/app/routes/user_protected.py
+++ user/app/routes/user_protected.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from user.app.database import get_db
 from user.app.dependencies import get_current_user
 from user.app.schemas.user import ResponseUser, UpdateUser
-from user.app.services.user import update_user
+from user.app.services.user import update_user, delete_user
 
 router = APIRouter(prefix="/protected")
 
@@ -18,3 +18,8 @@ async def read_current_user(current_user: ResponseUser = Depends(get_current_use
 async def update_current_user(updated_user: UpdateUser, db: Session = Depends(get_db),
                               current_user: ResponseUser = Depends(get_current_user)):
     return update_user(db, updated_user, current_user)
+
+
+@router.delete("/me/")
+async def delete_current_user(db: Session = Depends(get_db), current_user: ResponseUser = Depends(get_current_user)):
+    return delete_user(db, current_user)
