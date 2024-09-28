034cb310599bb8c840b771384a347cae3326740f
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 18:03:05 +0330

    added routes for retrive current user details

--- user/app/routes/user_protected.py
+++ user/app/routes/user_protected.py
@@ -1,3 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from user.app.dependencies import get_current_user
+from user.app.schemas.user import ResponseUser
 
 router = APIRouter(prefix="/protected")
+
+
+@router.get("/me/", response_model=ResponseUser)
+async def read_current_user(current_user: ResponseUser = Depends(get_current_user)):
+    return current_user
