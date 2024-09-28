8f0862fbdb8bcfe3c8f84dede3cc0a32d6d1fa56
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 14:34:19 +0330

    added protected routes to the main app

--- user/app/main.py
+++ user/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from user.app.routes import user_public
+from user.app.routes import user_public, user_protected
 
 app = FastAPI()
 
 app.include_router(user_public.router, prefix="/api")
+app.include_router(user_protected.router, prefix="/api")
