a5e036bf8ac0c3b9181734cc5366f8b1ad73b034
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 14:25:57 +0330

    registered login  to the router

--- user/app/routes/user.py
+++ user/app/routes/user.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from user.app.database import get_db
-from user.app.schemas.user import ResponseUser, RegisterUser
-from user.app.services.user import create_user
+from user.app.schemas.user import ResponseUser, RegisterUser, LoginUser
+from user.app.services.user import create_user, authenticate_user
 
 router = APIRouter()
 
@@ -11,3 +11,16 @@ router = APIRouter()
 @router.post("/register/", response_model=ResponseUser)
 async def register_user(user: RegisterUser, db: Session = Depends(get_db)):
     return create_user(db, user)
+
+
+@router.post("/login/")
+async def login_user(user: LoginUser, db: Session = Depends(get_db)):
+    token = authenticate_user(db, user.username, user.password)
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid username or password"
+        )
+
+    return token
