43b4f60a1294107f03b829327f8a38a73bbd8b89
arshia <arshiarezagholi1212@gmail.com>
2024-09-28 14:19:26 +0330

    implemented authenticate user service

--- user/app/services/user.py
+++ user/app/services/user.py
@@ -1,8 +1,11 @@
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
 from user.app.models.user import User
 from user.app.schemas.user import RegisterUser, ResponseUser
 from user.app.utils import hash_password
+from user.app.utils.hash_password import verify_password
+from user.app.utils.jwt import create_access_token
 
 
 async def create_user(db: Session, user: RegisterUser) -> ResponseUser:
@@ -21,3 +24,18 @@ async def create_user(db: Session, user: RegisterUser) -> ResponseUser:
     db.refresh(db_user)
 
     return ResponseUser.model_validate(db_user)
+
+
+async def authenticate_user(db: Session, username: str, password: str):
+    try:
+        db_user = db.query(User).filter(User.username == username).one()
+
+    except NoResultFound:
+        return None
+
+    if not verify_password(password, db_user.password):
+        return None
+
+    access_token = create_access_token({"sub": db_user.username})
+
+    return {"access_token": access_token, "token-type": "bearer"}
