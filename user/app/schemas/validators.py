2464773003a2a6d50fd24e01de5c77ebad0b61d4
arshia <arshiarezagholi1212@gmail.com>
2024-09-29 14:29:01 +0330

    validator functions with field_validator must be classmethod (changed)

--- user/app/schemas/validators.py
+++ user/app/schemas/validators.py
@@ -10,7 +10,8 @@ PHONE_NUMBER_REGEX = config.PHONE_NUMBER_REGEX
 
 class PasswordValidator(BaseModel):
     @field_validator('password')
-    def validate_password(self, password):
+    @classmethod
+    def validate_password(cls, password):
         if not re.match(PASSWORD_REGEX, password):
             raise ValidationError('Password must be at least 8 characters long and contain both letters and numbers')
         return password
@@ -18,7 +19,8 @@ class PasswordValidator(BaseModel):
 
 class PhoneNumberValidator(BaseModel):
     @field_validator('phone_number')
-    def validate_phone_number(self, phone_number):
+    @classmethod
+    def validate_phone_number(cls, phone_number):
         if phone_number and not re.match(PHONE_NUMBER_REGEX, phone_number):
             raise ValidationError('Phone number must be a valid Iranian phone number')
         return phone_number
