31a6718046d97f6819532c2a1397e2ec9ab294a9
arshia <arshiarezagholi1212@gmail.com>
2024-10-01 15:40:54 +0330

    added and initialized celery app

--- user/app/celery.py
+++ user/app/celery.py
@@ -1,3 +1,21 @@
 from celery import Celery
 
-celery_app = Celery("user_service")
+import config
+
+celery_app = Celery(
+    "user_service",
+    broker=config.CELERY_BROKER,
+    backend=config.CELERY_BACKEND,
+
+)
+
+celery_app.conf.update(
+    result_expires=config.CELERY_RESULT_EXPIRE,
+    task_serializer=config.CELERY_TASK_SERIALIZER,
+    accept_content=config.CELERY_ACCEPT_CONTENT,
+    result_serializer=config.CELERY_RESULT_SERIALIZER,
+    timezone=config.CELERY_TIMEZONE,
+    enable_utc=config.CELERY_ENABLE_UTC,
+)
+
+celery_app.autodiscover_tasks()
