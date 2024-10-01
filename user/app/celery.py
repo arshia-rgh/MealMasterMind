30ebf7a1478f3002eda26c528e2356b8fa183a2f
arshia <arshiarezagholi1212@gmail.com>
2024-10-01 15:31:38 +0330

    added celery file and celery configs in config file

---
+++ user/app/celery.py
@@ -0,0 +1,3 @@
+from celery import Celery
+
+celery_app = Celery("user_service")
--- user/app/config.py
+++ user/app/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+# DB configs
 class DBConfig:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -13,9 +14,21 @@ class DBConfig:
     DB_NAME = os.getenv("DB_NAME")
 
 
+# JWT token configs
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 20
 
+# Validations regex
 PHONE_NUMBER_REGEX = r"^(09|\+989)\d{9,10}$"
 PASSWORD_REGEX = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+
+# Celery configs
+CELERY_BROKER = os.getenv("CELERY_BROKER")
+CELERY_BACKEND = os.getenv("CELERY_BACKEND")
+CELERY_RESULT_EXPIRE = 3600
+CELERY_TASK_SERIALIZER = "json"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "Asia/Tehran"
+CELERY_ENABLE_UTC = True
