1ed73f794e266fa9e791b2fb8f31547a53c86230
arshia <arshiarezagholi1212@gmail.com>
2024-10-01 16:33:53 +0330

    added ConnectionConfig instance

--- user/app/tasks.py
+++ user/app/tasks.py
@@ -1,4 +1,18 @@
 from celery import shared_task
+from fastapi_mail import ConnectionConfig
+
+from user.app import config
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=config.MAIL_USERNAME,
+    MAIL_PASSWORD=config.MAIL_PASSWORD,
+    MAIL_PORT=config.MAIL_PORT,
+    MAIL_SERVER=config.MAIL_SERVER,
+    MAIL_FROM_NAME=config.MAIL_FROM_NAME,
+    MAIL_TLS=config.MAIL_TLS,
+    MAIL_SSL=config.MAIL_SSL,
+    USE_CREDENTIALS=config.USE_CREDENTIALS,
+)
 
 
 @shared_task
