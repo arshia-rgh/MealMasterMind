11188ec440cdfc4c122259320bcf6e3a0c355e07
arshia <arshiarezagholi1212@gmail.com>
2024-10-02 11:55:35 +0330

    implemented apply_migrations function to exec migrations files

--- mealPlanning/db/apply_migrations.go
+++ mealPlanning/db/apply_migrations.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func applyMigrations() error {
+	migrationsDir := "mealPlanning/db/versions"
+
+	files, err := os.ReadDir(migrationsDir)
+
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filePath := filepath.Join(migrationsDir, file.Name())
+		migrations, err := os.ReadFile(filePath)
+
+		if err != nil {
+			return err
+		}
+
+		_, err = DB.Exec(string(migrations))
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
