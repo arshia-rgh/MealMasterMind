8eba564dd9f6c40d78397d7b53a56cde44ec2fbb
arshia <arshiarezagholi1212@gmail.com>
2024-10-03 13:33:00 +0330

    added new migrations file

--- migrations/002_create_recipe_table.sql
+++ migrations/002_create_recipe_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS recipes (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL UNIQUE,
     instruction LONGTEXT NOT NULL,
     added_by INT,
     FOREIGN KEY (added_by) references users(id)
-)
+);
