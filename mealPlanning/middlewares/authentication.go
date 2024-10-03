9d9e7cf25b48a229915f525fda6810ef0b409bb1
arshia <arshiarezagholi1212@gmail.com>
2024-10-03 19:57:28 +0330

    separated the authentication middleware to three function (each function should do only one thing)

--- mealPlanning/middlewares/authentication.go
+++ mealPlanning/middlewares/authentication.go
@@ -12,14 +12,21 @@ func Authentication(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 
 	if token == "" {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
 		return
 	}
 
+	userID := getCurrentUser(token, context)
+
+	context.Set("user", userID)
+	context.Next()
+}
+
+func getCurrentUser(token string, context *gin.Context) int64 {
 	req, err := http.NewRequest("GET", "http://localhost:8000/api/protected/me/", nil)
 	if err != nil {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "err": err.Error()})
+		return 0
 	}
 
 	req.Header.Set("Authorization", token)
@@ -28,28 +35,34 @@ func Authentication(context *gin.Context) {
 	res, err := client.Do(req)
 
 	if err != nil || res.StatusCode != http.StatusOK {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "err": err})
+		return 0
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil || len(body) == 0 {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "err": err})
+		return 0
 	}
 
-	var responseData map[string]interface{}
-	if err = json.Unmarshal(body, &responseData); err != nil {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
+	responseData, err := responseBodyToString(body)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "err": err.Error()})
 	}
 
 	userID, ok := responseData["id"].(int64)
 	if !ok {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+		return 0
 	}
+	return userID
+}
 
-	context.Set("user", userID)
-	context.Next()
+func responseBodyToString(body []byte) (map[string]interface{}, error) {
+	var responseData map[string]interface{}
+	if err := json.Unmarshal(body, &responseData); err != nil {
+		return nil, err
+	}
+	return responseData, nil
 }
