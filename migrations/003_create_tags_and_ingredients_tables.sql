844df21c27e1d5acf8b6868bfe928348cb290690
arshia <arshiarezagholi1212@gmail.com>
2024-10-03 13:58:32 +0330

    edited migrations file 003

--- migrations/003_create_tags_and_ingredients_tables.sql
+++ migrations/003_create_tags_and_ingredients_tables.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS tags (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(255) NOT NULL UNIQUE,
+    recipe_id INT,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id)
 );
 
 CREATE TABLE IF NOT EXISTS ingredients (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(255) NOT NULL UNIQUE,
+    recipe_id INT,
+    FOREIGN KEY (recipe_id) REFERENCES recipe(id)
 );
