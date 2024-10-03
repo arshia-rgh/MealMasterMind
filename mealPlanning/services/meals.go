aa7e230679a5f2c6bb66a62250f78c69dc167aa9
arshia <arshiarezagholi1212@gmail.com>
2024-10-03 18:41:31 +0330

    added day to the Meal struct

--- mealPlanning/services/meals.go
+++ mealPlanning/services/meals.go
@@ -4,14 +4,15 @@ import "mealPlanning/db"
 
 type Meal struct {
 	ID         int64
+	Day        string
 	RecipeId   int
 	MealPlanId int
 }
 
 func (m *Meal) Save() error {
-	query := "INSERT INTO meals(recipe_id, meal_plan_id) VALUES (?, ?)"
+	query := "INSERT INTO meals(day,recipe_id, meal_plan_id) VALUES (?, ?, ?)"
 
-	result, err := db.DB.Exec(query, m.RecipeId, m.MealPlanId)
+	result, err := db.DB.Exec(query, m.Day, m.RecipeId, m.MealPlanId)
 	if err != nil {
 		return err
 	}
