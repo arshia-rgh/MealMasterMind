5c7edd98d535bfeb58e91a66c8c86562f9d62ab5
arshia <arshiarezagholi1212@gmail.com>
2024-10-03 18:40:10 +0330

    added Save method for the Meal

--- mealPlanning/services/meals.go
+++ mealPlanning/services/meals.go
@@ -1,11 +1,23 @@
 package services
 
+import "mealPlanning/db"
+
 type Meal struct {
-	ID         int
+	ID         int64
 	RecipeId   int
 	MealPlanId int
 }
 
-func (m *Meal) Save() {
-	query := "INSERT INTO meals()"
+func (m *Meal) Save() error {
+	query := "INSERT INTO meals(recipe_id, meal_plan_id) VALUES (?, ?)"
+
+	result, err := db.DB.Exec(query, m.RecipeId, m.MealPlanId)
+	if err != nil {
+		return err
+	}
+
+	ID, err := result.LastInsertId()
+	m.ID = ID
+
+	return err
 }
