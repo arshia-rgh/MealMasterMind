a240f4c2386029a25e55b06fc92714c27c78a0f0
arshia <arshiarezagholi1212@gmail.com>
2024-10-04 20:02:25 +0330

    added cors to the main server

--- mealPlanning/main.go
+++ mealPlanning/main.go
@@ -5,6 +5,8 @@ import (
 	"mealPlanning/middlewares"
 	"mealPlanning/routes"
 
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,24 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowAllOrigins:            false,
+		AllowOrigins:               nil,
+		AllowOriginFunc:            nil,
+		AllowOriginWithContextFunc: nil,
+		AllowMethods:               nil,
+		AllowPrivateNetwork:        false,
+		AllowHeaders:               nil,
+		AllowCredentials:           false,
+		ExposeHeaders:              nil,
+		MaxAge:                     0,
+		AllowWildcard:              false,
+		AllowBrowserExtensions:     false,
+		CustomSchemas:              nil,
+		AllowWebSockets:            false,
+		AllowFiles:                 false,
+		OptionsResponseStatusCode:  0,
+	}))
 
 	protectedGroup := server.Group("/api/protected")
 	protectedGroup.Use(middlewares.Authentication)
