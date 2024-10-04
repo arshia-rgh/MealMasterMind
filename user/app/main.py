710f58716461791649a5e3bf51cf862df86aca55
arshia <arshiarezagholi1212@gmail.com>
2024-10-04 21:41:22 +0330

    added cors for other services

--- user/app/main.py
+++ user/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from user.app.routes import user_protected, user_public
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Origin", "Content-Length", "Content-Type", "Authorization", "Accept"],
+    max_age=12 * 60,
+)
 
 app.include_router(user_public.router, prefix="/api")
 app.include_router(user_protected.router, prefix="/api")
