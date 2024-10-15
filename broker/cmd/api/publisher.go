5615de42937d6bfdcd26bc9c1a84eba4afa09630
arshia <arshiarezagholi1212@gmail.com>
2024-10-15 23:53:10 +0330

    added the rabbitmq connection func

--- broker/cmd/api/publisher.go
+++ broker/cmd/api/publisher.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
+func publishMessage(queueName string, message []byte) error {
+
+}
+
+func connect() (*amqp.Connection, error) {
+	var connection *amqp.Connection
+	var counts int64
+	var backOff = 1 * time.Second
+
+	rabbitMQURL := fmt.Sprintf("amqp://%v:%v@%v:%v/",
+		os.Getenv("RABBITMQ_USERNAME"),
+		os.Getenv("RABBITMQ_PASSWORD"),
+		os.Getenv("RABBITMQ_HOST"),
+		os.Getenv("RABBITMQ_PORT"),
+	)
+
+	for {
+		c, err := amqp.Dial(rabbitMQURL)
+		if err != nil {
+			log.Println("rabbitmq not yet ready...!")
+			counts++
+		} else {
+			log.Println("connected to RabbitMQ")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Printf("backing off for %v seconds\n", backOff)
+		time.Sleep(backOff)
+
+	}
+
+	return connection, nil
+}
