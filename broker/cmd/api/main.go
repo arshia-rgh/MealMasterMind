346262408535397c694a29dfa297c196d17fad93
arshia <arshiarezagholi1212@gmail.com>
2024-10-15 15:51:19 +0330

    added the gin server and cors in the broker

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
+func main() {
+	srv := gin.Default()
+
+	srv.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Accept", "Authorization", "content-type", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 }
