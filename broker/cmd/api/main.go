540f827d983df64de11aacf2027c720f17768218
arshia <arshiarezagholi1212@gmail.com>
2024-10-15 16:19:44 +0330

    moved routes to a new file

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
-	"time"
 )
 
 const webPort = "8080"
@@ -14,23 +11,11 @@ const webPort = "8080"
 func main() {
 	srv := gin.Default()
 
-	srv.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Accept", "Authorization", "content-type", "X-CSRF-Token"},
-		ExposeHeaders:    []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-
-	srv.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"status": "alive",
-		})
-	})
+	registerRoutes(srv)
 
 	err := srv.Run(fmt.Sprintf(":%v", webPort))
 	if err != nil {
 		log.Panic(err)
 	}
+
 }
