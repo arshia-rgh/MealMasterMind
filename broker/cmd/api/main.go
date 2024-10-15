8d9933526ed8d05c2e173143c1d7f02d93b10ea1
arshia <arshiarezagholi1212@gmail.com>
2024-10-15 16:14:37 +0330

    added the ping route for the broker

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 	"time"
 )
+
+const webPort = "8080"
 
 func main() {
 	srv := gin.Default()
@@ -17,4 +22,15 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+
+	srv.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"status": "alive",
+		})
+	})
+
+	err := srv.Run(fmt.Sprintf(":%v", webPort))
+	if err != nil {
+		log.Panic(err)
+	}
 }
