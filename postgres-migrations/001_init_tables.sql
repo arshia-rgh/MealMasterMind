cff9f9ff4811b144553f36931aa70e2d13314852
arshia <arshiarezagholi1212@gmail.com>
2024-10-16 21:23:02 +0330

    changed migrations to suitable for postgres

--- postgres-migrations/001_init_tables.sql
+++ postgres-migrations/001_init_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id           INT PRIMARY KEY AUTO_INCREMENT,
+    id           SERIAL PRIMARY KEY ,
     first_name   VARCHAR(255) NULL,
     last_name    VARCHAR(255) NULL,
     username     VARCHAR(255) UNIQUE,
@@ -11,16 +11,16 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS recipes
 (
-    id          INT PRIMARY KEY AUTO_INCREMENT,
+    id          SERIAL PRIMARY KEY ,
     name        VARCHAR(255) NOT NULL UNIQUE,
-    instruction LONGTEXT     NOT NULL,
+    instruction TEXT     NOT NULL,
     added_by    INT,
     FOREIGN KEY (added_by) references users (id)
 );
 
 CREATE TABLE IF NOT EXISTS tags
 (
-    id        INT PRIMARY KEY AUTO_INCREMENT,
+    id        SERIAL PRIMARY KEY ,
     name      VARCHAR(255) NOT NULL UNIQUE,
     recipe_id INT,
     FOREIGN KEY (recipe_id) REFERENCES recipes (id)
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS tags
 
 CREATE TABLE IF NOT EXISTS ingredients
 (
-    id        INT PRIMARY KEY AUTO_INCREMENT,
+    id        SERIAL PRIMARY KEY ,
     name      VARCHAR(255) NOT NULL UNIQUE,
     recipe_id INT,
     FOREIGN KEY (recipe_id) REFERENCES recipes (id)
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS ingredients
 
 CREATE TABLE IF NOT EXISTS dietary_restrictions
 (
-    id   INT PRIMARY KEY AUTO_INCREMENT,
+    id   SERIAL PRIMARY KEY ,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS user_dietary_restrictions
 
 CREATE TABLE IF NOT EXISTS meal_plans
 (
-    id      INT PRIMARY KEY AUTO_INCREMENT,
+    id      SERIAL PRIMARY KEY ,
     user_id INT,
     name    VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users (id)
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS meal_plans
 
 CREATE TABLE IF NOT EXISTS meals
 (
-    id           INT PRIMARY KEY AUTO_INCREMENT,
+    id           SERIAL PRIMARY KEY ,
     day          VARCHAR(100) NOT NULL,
     recipe_id    INT,
     meal_plan_id INT,
