86c8759c0b88d555a3ef03df5e0d7d97015b609b
arshia <arshiarezagholi1212@gmail.com>
2024-10-16 10:24:59 +0330

    added a service queues for all services queues name

--- listener/event/consumer.go
+++ listener/event/consumer.go
@@ -1,1 +1,13 @@
 package main
+
+var serviceQueues = map[string][]string{
+	"auth": {
+		"auth_login",
+		"auth_register",
+		"auth_reset_password",
+		"auth_change_password",
+		"auth_delete",
+		"auth_update",
+	},
+	// Other services
+}
