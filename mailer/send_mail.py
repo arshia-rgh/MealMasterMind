4829a198c3c70a915edd3b497d5b4a512dbe1986
arshia <arshiarezagholi1212@gmail.com>
2024-10-18 02:33:01 +0330

    added a html file to render

--- mailer/send_mail.py
+++ mailer/send_mail.py
@@ -1,4 +1,6 @@
+import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -11,3 +13,19 @@ msg["Subject"] = config.subject
 
 env = Environment(loader=FileSystemLoader("/templates"))
 template = env.get_template("email.html")
+
+context = {
+    "link":  # TODO: Get from gRPC
+    "subject": # TODO: Get from gRPC
+    "email": # TODO: Get from gRPC
+}
+
+html_content = template.render(context)
+
+html_part = MIMEText(html_content, "html")
+msg.attach(html_part)
+
+with smtplib.SMTP(config.smtp_server, config.smtp_port) as server:
+    server.starttls()
+    server.login(config.smtp_user, config.smtp_password)
+    server.sendmail(config.from_email, config.to_email, msg.as_string())
