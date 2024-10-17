44a738e0c84562430fa825afa4008362090dcca4
arshia <arshiarezagholi1212@gmail.com>
2024-10-18 00:06:25 +0330

    added the publish message function

--- user/event/publish.py
+++ user/event/publish.py
@@ -2,13 +2,38 @@ import logging
 import math
 import os
 import time
+from typing import Tuple
 
 import pika
+from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
 
 logging.basicConfig(level=logging.INFO)
 
 
-def connect():
+def publish_message(routing_key: str, data: dict):
+    conn, ch = connect()
+    if not conn or not ch:
+        return
+    try:
+        ch.queue_declare(queue=routing_key)
+        ch.basic_publish(
+            exchange="",
+            routing_key=routing_key,
+            body=bytes(data),
+            properties=pika.BasicProperties(content_type="application/json"),
+        )
+        logging.info(f"published message: {data} to queue: {routing_key}")
+    except Exception as e:
+        logging.error(f"Failed to publish message: {e}")
+
+    finally:
+        if ch:
+            ch.close()
+        if conn:
+            conn.close()
+
+
+def connect() -> Tuple[None, None] | Tuple[BlockingConnection, BlockingChannel]:
     counts = 0
 
     rabbitmq_url = f"amqp://{os.getenv('RABBITMQ_USERNAME')}:{os.getenv('RABBITMQ_PASSWORD')}@{os.getenv('RABBITMQ_HOST')}:{os.getenv('RABBITMQ_PORT')}/"
@@ -25,7 +50,7 @@ def connect():
 
             if counts > 5:
                 logging.error(f"Failed to connect to RabbitMQ: {e}")
-                return
+                return None, None
 
             back_off = math.pow(counts, 2)
             logging.info(f"Backing off for {back_off} seconds")
