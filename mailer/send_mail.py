a6afffafe0261e8500d61f7c19fce820830f406d
arshia <arshiarezagholi1212@gmail.com>
2024-10-18 02:24:47 +0330

    added jinja2 env

--- mailer/send_mail.py
+++ mailer/send_mail.py
@@ -1,8 +1,13 @@
 from email.mime.multipart import MIMEMultipart
 
+from jinja2 import Environment, FileSystemLoader
+
 import config
 
 msg = MIMEMultipart()
 msg["From"] = config.from_email
 msg["To"] = config.to_email
 msg["Subject"] = config.subject
+
+env = Environment(loader=FileSystemLoader("/templates"))
+template = env.get_template("email.html")
