6ad6b8ff7e37b2c0f8c40b32ee3652316358d5ca
arshia <arshiarezagholi1212@gmail.com>
2024-10-18 03:12:50 +0330

    implemented the send mail func and class for the gRPC

--- mailer/send_mail.py
+++ mailer/send_mail.py
@@ -1,31 +1,47 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from jinja2 import Environment, FileSystemLoader
 
-import config
+from grpc.mails_pb2 import MailResponseResetLink
+from grpc.mails_pb2_grpc import SendMailServicer
 
-msg = MIMEMultipart()
-msg["From"] = config.from_email
-msg["To"] = config.to_email
-msg["Subject"] = config.subject
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_user = os.getenv("MAIL_USERNAME")
+smtp_password = os.getenv("MAIL_PASSWORD")
 
-env = Environment(loader=FileSystemLoader("/templates"))
-template = env.get_template("email.html")
+from_email = os.getenv("MAIL_USERNAME")
 
-context = {
-    "link":  # TODO: Get from gRPC
-    "subject": # TODO: Get from gRPC
-    "email": # TODO: Get from gRPC
-}
 
-html_content = template.render(context)
+class SendMail(SendMailServicer):
 
-html_part = MIMEText(html_content, "html")
-msg.attach(html_part)
+    def SendMail(self, request, context):
+        msg = MIMEMultipart()
+        msg["From"] = from_email
+        msg["To"] = request.email
+        msg["Subject"] = request.subject
 
-with smtplib.SMTP(config.smtp_server, config.smtp_port) as server:
-    server.starttls()
-    server.login(config.smtp_user, config.smtp_password)
-    server.sendmail(config.from_email, config.to_email, msg.as_string())
+        env = Environment(loader=FileSystemLoader("/templates"))
+        template = env.get_template("email.html")
+
+        context = {
+            "link": request.link,
+            "subject": request.subject,
+            "email": request.email,
+        }
+
+        html_content = template.render(context)
+
+        html_part = MIMEText(html_content, "html")
+        msg.attach(html_part)
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_user, smtp_password)
+            server.sendmail(from_email, request.email, msg.as_string())
+
+        response = MailResponseResetLink(result="Email sent successfully")
+        return response
