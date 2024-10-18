6b11d7fc31b05ff47c88002bf531ab7dd1252f54
arshia <arshiarezagholi1212@gmail.com>
2024-10-18 11:46:17 +0330

    used the send mail request GRpc in the consume function

--- listener/event/consumer.go
+++ listener/event/consumer.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"listener/mails"
 	"log"
 )
+
+type MailRequest struct {
+	Email   string `json:"email"`
+	Subject string `json:"subject"`
+	Link    string `json:"link"`
+}
 
 func consume(routingKeys []string, conn *amqp.Connection) error {
 	ch, err := conn.Channel()
@@ -45,7 +53,14 @@ func consume(routingKeys []string, conn *amqp.Connection) error {
 			for d := range msgs {
 				switch routingKey {
 				case "send-mail":
-					//TODO gRPC to the mail service
+					var mailReq MailRequest
+					if err := json.Unmarshal(d.Body, &mailReq); err != nil {
+						log.Println("Failed to unmarshal JSON", err)
+						continue
+					}
+					if err := mails.RequestToSendMailGRPC(mailReq.Email, mailReq.Subject, mailReq.Link); err != nil {
+						log.Println("Failed to send mail:", err)
+					}
 				case "log":
 					//TODO gRPC to the logger service
 
