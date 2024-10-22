1d461f7b9486eaf9c3f6e455e2deb29987f49fda
arshia <arshiarezagholi1212@gmail.com>
2024-10-22 18:58:56 +0330

    removed grpc from the mailer service

--- mailer/main.py
+++ mailer/main.py
@@ -1,17 +1,5 @@
-import grpc
-from concurrent import futures
-
-from mails.mails_pb2_grpc import add_SendMailServicer_to_server
-from send_mail import SendMail
-
-
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-
-    add_SendMailServicer_to_server(SendMail(), server)
-    server.add_insecure_port("[::]:50051")
-    server.start()
-    server.wait_for_termination()
+    pass
 
 
 if __name__ == "__main__":
--- mailer/send_mail.py
+++ mailer/send_mail.py
@@ -5,9 +5,6 @@ from email.mime.text import MIMEText
 
 from jinja2 import Environment, FileSystemLoader
 
-from mails.mails_pb2 import MailResponseResetLink
-from mails.mails_pb2_grpc import SendMailServicer
-
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
 smtp_user = os.getenv("MAIL_USERNAME")
@@ -16,32 +13,27 @@ smtp_password = os.getenv("MAIL_PASSWORD")
 from_email = os.getenv("MAIL_USERNAME")
 
 
-class SendMail(SendMailServicer):
+def SendMail():
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = request.email
+    msg["Subject"] = request.subject
 
-    def SendMail(self, request, context):
-        msg = MIMEMultipart()
-        msg["From"] = from_email
-        msg["To"] = request.email
-        msg["Subject"] = request.subject
+    env = Environment(loader=FileSystemLoader("./templates"))
+    template = env.get_template("email.html")
 
-        env = Environment(loader=FileSystemLoader("./templates"))
-        template = env.get_template("email.html")
+    context = {
+        "link": request.link,
+        "subject": request.subject,
+        "email": request.email,
+    }
 
-        context = {
-            "link": request.link,
-            "subject": request.subject,
-            "email": request.email,
-        }
+    html_content = template.render(context)
 
-        html_content = template.render(context)
+    html_part = MIMEText(html_content, "html")
+    msg.attach(html_part)
 
-        html_part = MIMEText(html_content, "html")
-        msg.attach(html_part)
-
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_user, smtp_password)
-            server.sendmail(from_email, request.email, msg.as_string())
-
-        response = MailResponseResetLink(result="Email sent successfully")
-        return response
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        server.sendmail(from_email, request.email, msg.as_string())
