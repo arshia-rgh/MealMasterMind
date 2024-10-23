91ef0f64ed8312e4d7ee90cb59d435ecac26821c
arshia <arshiarezagholi1212@gmail.com>
2024-10-24 02:47:01 +0330

    check for if message sent successfully in publish_message function added

--- user/event/publish.py
+++ user/event/publish.py
@@ -3,11 +3,12 @@ import os
 from rabbit_helper.rabbit import Rabbit
 
 
-async def publish_message(routing_key: str, data: dict):
+async def publish_message(routing_key: str, data: dict) -> bool:
     rabbitmq_url = f"amqp://{os.getenv('RABBITMQ_USERNAME')}:{os.getenv('RABBITMQ_PASSWORD')}@{os.getenv('RABBITMQ_HOST')}:{os.getenv('RABBITMQ_PORT')}/"
     rabbit = Rabbit(rabbitmq_url)
 
     try:
-        await rabbit.publish(routing_key, data)
+        ok = await rabbit.publish(routing_key, data)
+        return ok
     finally:
         rabbit.close()
