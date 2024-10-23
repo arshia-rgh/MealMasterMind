3e15cecec26deb25b8760a8ffb627a17b6ecf5b9
arshia <arshiarezagholi1212@gmail.com>
2024-10-23 15:04:03 +0330

    added a middleware to log request and responses for auth

--- user/app/main.py
+++ user/app/main.py
@@ -1,6 +1,7 @@
 from app.config import origins
 from app.routes import user_protected, user_public
-from fastapi import FastAPI
+from event.publish import publish_message
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,6 +14,20 @@ app.add_middleware(
     allow_headers=["Origin", "Content-Length", "Content-Type", "Authorization", "Accept"],
     max_age=12 * 60,
 )
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    # Log the request
+    await publish_message("logs", {"name": "auth", "level": "info", "data": f"Request: {request.method} {request.url}"})
+
+    response = await call_next(request)
+
+    # Log the response
+    await publish_message("logs", {"name": "auth", "level": "info", "data": f"Response status: {response.status_code}"})
+
+    return response
+
 
 app.include_router(user_public.router, prefix="/api")
 app.include_router(user_protected.router, prefix="/api")
