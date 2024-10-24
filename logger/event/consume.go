f8afb4787caa50a5758cc0dca5d637187d027706
arshia <arshiarezagholi1212@gmail.com>
2024-10-24 16:20:57 +0330

    added the Consume func in the logger service

--- logger/event/consume.go
+++ logger/event/consume.go
@@ -1,1 +1,25 @@
 package event
+
+import (
+	"fmt"
+	Rabbit "github.com/arshia-rgh/rabbit-helper-go/rabbit_helper"
+	"os"
+)
+
+func Consume(routingKey string, callback func(data any)) {
+	rabbitMQURL := fmt.Sprintf("amqp://%v:%v@%v:%v/",
+		os.Getenv("RABBITMQ_USERNAME"),
+		os.Getenv("RABBITMQ_PASSWORD"),
+		os.Getenv("RABBITMQ_HOST"),
+		os.Getenv("RABBITMQ_PORT"),
+	)
+
+	rabbit := Rabbit.New(rabbitMQURL)
+
+	go func() {
+		err := rabbit.Consume(routingKey, callback)
+		if err != nil {
+			return
+		}
+	}()
+}
