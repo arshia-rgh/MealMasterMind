8cead1b5ebeb2523d0325c927296b0b2a92f56e7
arshia <arshiarezagholi1212@gmail.com>
2024-10-29 01:24:55 +0330

    changed port for gRPC client in the mealplanning service

--- mealPlanning/cmd/api/middlewares/authentication.go
+++ mealPlanning/cmd/api/middlewares/authentication.go
@@ -32,7 +32,7 @@ func Authentication(context *gin.Context) {
 }
 
 func getCurrentUser(token string) (int64, error) {
-	conn, err := grpc.NewClient("auth-service:50001",
+	conn, err := grpc.NewClient("auth-service:50051",
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithConnectParams(grpc.ConnectParams{MinConnectTimeout: 5 * time.Second}),
 	)
