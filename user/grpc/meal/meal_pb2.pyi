7a332324a90baa852b2135bd58d52666c5201406
arshia <arshiarezagholi1212@gmail.com>
2024-10-29 01:02:43 +0330

    added the grpc generated files for the user service

---
+++ user/grpc/meal/meal_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class AuthReq(_message.Message):
+    __slots__ = ("token",)
+    TOKEN_FIELD_NUMBER: _ClassVar[int]
+    token: str
+    def __init__(self, token: _Optional[str] = ...) -> None: ...
+
+class AuthRes(_message.Message):
+    __slots__ = ("userID",)
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    userID: int
+    def __init__(self, userID: _Optional[int] = ...) -> None: ...
