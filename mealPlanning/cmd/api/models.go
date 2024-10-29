f76af7fdcb4cf53025dbc6cb97b6ed92db528ce0
arshia <arshiarezagholi1212@gmail.com>
2024-10-29 16:43:04 +0330

    added the GetByID for the Meal

--- mealPlanning/cmd/api/models.go
+++ mealPlanning/cmd/api/models.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"context"
+	"time"
 )
 
+type Models struct {
+	Meal     Meal
+	MealPlan MealPlan
+}
+
+const dbTimeout = time.Second * 3
+
 type Meal struct {
-	ID         int64  `json:"id"`
-	Day        string `json:"day" binding:"required"`
-	RecipeId   int    `json:"recipe_id" binding:"required"`
-	MealPlanId int    `json:"meal_plan_id" binding:"required"`
+	ID         int64  `json:"id,omitempty"`
+	Day        string `json:"day,omitempty"`
+	RecipeId   int    `json:"recipe_id,omitempty"`
+	MealPlanId int    `json:"meal_plan_id,omitempty"`
 }
 
 type MealPlan struct {
-	ID     int64  `json:"id"`
-	UserID int64  `json:"user_id"`
-	Name   string `json:"name" binding:"required"`
+	ID     int64  `json:"id,omitempty"`
+	UserID int64  `json:"user_id,omitempty"`
+	Name   string `json:"name,omitempty"`
 }
 
 func (m *Meal) Save() error {
@@ -23,4 +31,22 @@ func (m *Meal) Save() error {
 	err := DB.QueryRowContext(context.TODO(), query, m.Day, m.RecipeId, m.MealPlanId).Scan(&m.ID)
 
 	return err
+}
+
+func (m *Meal) GetByID(ID int64) (*Meal, error) {
+	query := "SELECT * FROM meals WHERE id = ?"
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+
+	defer cancel()
+
+	var meal Meal
+
+	err := DB.QueryRowContext(ctx, query, ID).Scan(&meal.ID, &meal.Day, &meal.RecipeId, &meal.MealPlanId)
+
+	if err != nil {
+		return nil, err
+	}
+	return &meal, nil
+
 }
