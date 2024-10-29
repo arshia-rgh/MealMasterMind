34b52d7d24736e843c27ae55a416e00e6acf62e3
arshia <arshiarezagholi1212@gmail.com>
2024-10-29 22:31:48 +0330

    added the publish for logs in the meal service

--- mealPlanning/cmd/api/handlers.go
+++ mealPlanning/cmd/api/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"mealPlanning/event"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +15,11 @@ func createMeal(context *gin.Context) {
 	err := context.ShouldBindJSON(&meal)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "invalid data", "err": err.Error()})
+		go event.Publish("logs", map[string]string{
+			"name":  "meal",
+			"level": "error",
+			"data":  fmt.Sprintf("failed to create meal: %v", err.Error()),
+		})
 		return
 	}
 
@@ -20,9 +27,19 @@ func createMeal(context *gin.Context) {
 	if err != nil {
 		log.Printf("Server error: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "server error"})
+		go event.Publish("logs", map[string]string{
+			"name":  "meal",
+			"level": "error",
+			"data":  fmt.Sprintf("failed to create meal: %v", err.Error()),
+		})
 		return
 	}
 
+	go event.Publish("logs", map[string]string{
+		"name":  "meal",
+		"level": "info",
+		"data":  fmt.Sprintf("meal created successfully: %v", meal),
+	})
 	context.JSON(http.StatusCreated, gin.H{"message": "meal created successfully", "meal": meal})
 
 }
