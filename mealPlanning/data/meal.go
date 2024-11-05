297407d2e92473bb00ca5d84a1eea20d9bf5d4b5
arshia <arshiarezagholi1212@gmail.com>
2024-11-05 21:08:17 +0330

    implemented the models struct

--- mealPlanning/data/meal.go
+++ mealPlanning/data/meal.go
@@ -1,1 +1,8 @@
 package data
+
+type Meal struct {
+	ID         int64  `json:"id,omitempty"`
+	Day        string `json:"day,omitempty"`
+	RecipeId   int    `json:"recipe_id,omitempty"`
+	MealPlanId int    `json:"meal_plan_id,omitempty"`
+}
--- mealPlanning/data/meal_plan.go
+++ mealPlanning/data/meal_plan.go
@@ -1,1 +1,7 @@
 package data
+
+type MealPlan struct {
+	ID     int64  `json:"id,omitempty"`
+	UserID int64  `json:"user_id,omitempty"`
+	Name   string `json:"name,omitempty"`
+}
