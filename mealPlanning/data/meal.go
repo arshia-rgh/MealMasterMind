012c97a6687b18c38ac7489880418851f3d95c94
arshia <arshiarezagholi1212@gmail.com>
2024-11-05 21:13:36 +0330

    moved Meal methods to the meal file

--- mealPlanning/data/meal.go
+++ mealPlanning/data/meal.go
@@ -1,8 +1,82 @@
 package data
 
+import "context"
+
 type Meal struct {
 	ID         int64  `json:"id,omitempty"`
 	Day        string `json:"day,omitempty"`
 	RecipeId   int    `json:"recipe_id,omitempty"`
 	MealPlanId int    `json:"meal_plan_id,omitempty"`
+}
+
+func (m *Meal) Save() error {
+	query := "INSERT INTO meals(day, recipe_id, meal_plan_id) VALUES ($1, $2, $3) RETURNING id"
+
+	err := db.QueryRowContext(context.TODO(), query, m.Day, m.RecipeId, m.MealPlanId).Scan(&m.ID)
+
+	return err
+}
+
+func (m *Meal) GetByID(ID int64) (*Meal, error) {
+	query := "SELECT * FROM meals WHERE id = ?"
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+
+	defer cancel()
+
+	var meal Meal
+
+	err := db.QueryRowContext(ctx, query, ID).Scan(&meal.ID, &meal.Day, &meal.RecipeId, &meal.MealPlanId)
+
+	if err != nil {
+		return nil, err
+	}
+	return &meal, nil
+
+}
+
+func (m *Meal) GetAll() ([]*Meal, error) {
+	query := "SELECT * FROM meals"
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var meals []*Meal
+	for rows.Next() {
+		var meal Meal
+		err := rows.Scan(&meal.ID, &meal.Day, &meal.RecipeId, &meal.MealPlanId)
+		if err != nil {
+			return nil, err
+		}
+		meals = append(meals, &meal)
+
+	}
+
+	return meals, nil
+
+}
+
+func (m *Meal) Delete(ID int64) error {
+	query := "DELETE FROM meals WHERE id = ?"
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	_, err := db.ExecContext(ctx, query, ID)
+
+	return err
+
+}
+
+func (m *Meal) Update(ID int64) error {
+	query := "UPDATE meals SET day = ?, recipe_id = ?, meal_plan_id = ? WHERE id = ?"
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	_, err := db.ExecContext(ctx, query, ID)
+
+	return err
 }
