2825b98810a1714136c6139bc9968a816ae80b61
arshia <arshiarezagholi1212@gmail.com>
2024-11-05 21:13:20 +0330

    added the Models struct constructor method

--- mealPlanning/data/models.go
+++ mealPlanning/data/models.go
@@ -1,6 +1,20 @@
 package data
 
+import (
+	"database/sql"
+	"time"
+)
+
+var db *sql.DB
+
+const dbTimeout = time.Second * 3
+
 type Models struct {
 	Meal     Meal
 	MealPlan MealPlan
+}
+
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+	return Models{}
 }
