8e45602795b24b7a7ec165c4222e3d5250068da5
arshia <arshiarezagholi1212@gmail.com>
2024-11-06 22:16:57 +0330

    added the getMeal to the router

--- mealPlanning/cmd/api/routers.go
+++ mealPlanning/cmd/api/routers.go
@@ -7,6 +7,7 @@ import (
 func RegisterRoutesProtected(server *gin.RouterGroup) {
 	server.POST("/meals", createMeal)
 	server.GET("/meals", getMeals)
+	server.GET("/meals/:id", getMeal)
 }
 
 func RegisterRoutesPublic(server *gin.RouterGroup) {
