87197a9c0b29934059c5bef8a2c9195184ad3aa0
arshia <arshiarezagholi1212@gmail.com>
2024-11-08 00:44:44 +0330

    registered the updateMeal handler to the router

--- mealPlanning/cmd/api/routers.go
+++ mealPlanning/cmd/api/routers.go
@@ -8,6 +8,7 @@ func RegisterRoutesProtected(server *gin.RouterGroup) {
 	server.POST("/meals", createMeal)
 	server.GET("/meals", getMeals)
 	server.GET("/meals/:id", getMeal)
+	server.PUT("/meals/:id", updateMeal)
 }
 
 func RegisterRoutesPublic(server *gin.RouterGroup) {
